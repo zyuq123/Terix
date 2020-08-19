f225351b93c87f6d3355378b81e6ae79d4686650
1170442029 <69881561+1170442029@users.noreply.github.com>
2020-08-19 14:47:54 +0800

    修改

--- hello.cs
+++ hello.cs
@@ -3,9 +3,7 @@ using System;
 namespace Github {
     class Program {
         static void Main (string[] args) { 
-            System.Console.WriteLine("Hello C#");
-            //
-            int a = 30;
         }
+            Sy
     }
 }
--- hello1.cs
+++ hello1.cs
@@ -2,10 +2,11 @@ using System;
 
 namespace Github {
     class Program {
-        static void Main (string[] args) { 
-            System.Console.WriteLine("Hello C#");
-            //
-            int a = 30;
+        
+
+
+
+          
         }
     }
 }
