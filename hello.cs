8dfffe44f8ff32dfbdd41ca12d0ab6f20c185a07
zyuq123 <zyuq123@126.com>
2020-08-19 11:19:57 +0800

    第二次修改
    XXXXX

--- hello.cs
+++ hello.cs
@@ -4,6 +4,8 @@ namespace Github {
     class Program {
         static void Main (string[] args) { 
             System.Console.WriteLine("Hello C#");
+            //
+            int a = 30;
         }
     }
 }
